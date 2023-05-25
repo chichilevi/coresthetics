0bcd5e2452c424a6ec4090cc6b11bb4e2bc3c13a
KaiMi777 <info@iliascc.co.za>
2023-05-25 05:33:19 +0200

    1st push

---
+++ _data/metadata.js
@@ -0,0 +1,31 @@
+module.exports = {
+  title: "Coresthetics | Fitness Reimagined.",
+  url: "https://www.core-sthetics.co.za",
+  language: "en",
+  domain: "core-sthetics.co.za",
+  description: "Coresthetics | Fitness training that adapts to your goals.",
+  genre: "Gym, Fitnes",
+  googleAnalyticsId: "GA ID. Update me. Note, that this is not compatible with the not-yet-commonly used version 4 of Google Analytics",
+  sendWebVitals: false,
+  feed: {
+    subtitle: "Subtitle. Update me.",
+    filename: "feed.xml",
+    path: "/feed/feed.xml",
+    id: "https://update-me.com"
+  },
+  jsonfeed: {
+    path: "/feed/feed.json",
+    url: "https://update-me.com/feed/feed.json"
+  },
+  author: {
+    name: "Kurt Mbanje.",
+    email: "kurt@core-sthetics.co.za",
+    url: "https://www.mbanje.co.za/kurt/",
+    twitterHandle: "update_me"
+  },
+  publisher: {
+    name: "Kai Mbanje."
+  }
+}
+
+
